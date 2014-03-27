ebaa2b4fe1487e8ebfb7e6181a62e2fdd96d42ca
wangqianyue <790433996@qq.com>
2014-03-27 20:08:28 +0800

    header.php

--- application/views/header.php
+++ application/views/header.php
@@ -12,66 +12,74 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/stylesheets/font-awesome.css" />   
         <script src="<?php echo base_url();?>assets/js/jquery-1.11.0.min.js" type="text/javascript"></script>
         <script src="<?php echo base_url();?>assets/js/bootstrap.js" type="text/javascript"></script>
-            <style type="text/css">
-                #line-chart {
-                    height:300px;
-                    width:800px;
-                    margin: 0px auto;
-                    margin-top: 1em;
-                }
-                .brand { font-family: georgia, serif; }
-                .brand .first {
-                    color: #ccc;
-                    font-style: italic;
-                }
-                .brand .second {
-                    color: #fff;
-                    font-weight: bold;
-                }
-            </style>
+    <style type="text/css">
+        #line-chart {
+            height:300px;
+            width:800px;
+            margin: 0px auto;
+            margin-top: 1em;
+        }
+        .brand { font-family: georgia, serif; }
+        .brand .first {
+            color: #ccc;
+            font-style: italic;
+        }
+        .brand .second {
+            color: #fff;
+            font-weight: bold;
+        }
+    </style>
 </head>
+<body>
+<html>
     <div class="navbar">
         <div class="navbar-inner">
-            <div class="container-fluid">
                 <ul class="nav pull-right">
-                    
+
+                    <li><a href="#" class="hidden-phone visible-tablet visible-desktop" role="button">Settings</a></li>
                     <li id="fat-menu" class="dropdown">
-                        <a href="#" id="drop3" role="button" class="dropdown-toggle" data-toggle="dropdown">
+                        <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">
                             <i class="icon-user"></i> Jack Smith
                             <i class="icon-caret-down"></i>
                         </a>
+
                         <ul class="dropdown-menu">
-                            <li><a tabindex="-1" href="#">Settings</a></li>
+                            <li><a tabindex="-1" href="#">My Account</a></li>
                             <li class="divider"></li>
+                            <li><a tabindex="-1" class="visible-phone" href="#">Settings</a></li>
+                            <li class="divider visible-phone"></li>
                             <li><a tabindex="-1" href="sign-in.html">Logout</a></li>
                         </ul>
                     </li>
+
                 </ul>
                 <a class="brand" href="index.html"><span class="first">Your</span> <span class="second">Company</span></a>
-            </div>
         </div>
     </div>
-<body>
-<html>
-    <div class="container-fluid">
-        
-        <div class="row-fluid">
-            <div class="span3">
-                <div class="sidebar-nav">
-                  <div class="nav-header" data-toggle="collapse" data-target="#dashboard-menu"><i class="icon-dashboard"></i>Dashboard</div>
-                    <ul id="dashboard-menu" class="nav nav-list collapse in">
-                        <li><a href="index.html">Home</a></li>
-                        <li ><a href="users.html">Sample List</a></li>
-                        <li ><a href="user.html">Sample Item</a></li>
-                        <li ><a href="gallery.html">Gallery</a></li>
-                        <li ><a href="calendar.html">Calendar</a></li>
-                        <li ><a href="faq.html">文章</a></li>
-                        <li ><a href="help.html">Help</a></li>
-                        
-                    </ul>
-        		</div>
-            </div>
-        </div>
+<div class="sidebar-nav">
+        <!--
+        <form class="search form-inline">
+            <input type="text" placeholder="Search...">
+        </form>
+        -->
+
+        <a href="#dashboard-menu" class="nav-header" data-toggle="collapse"><i class="icon-dashboard"></i>后台管理</a>
+        <ul id="dashboard-menu" class="nav nav-list collapse in">
+            <li><a href="index.html">主页</a></li>
+            <li><a href="index.html">文章管理</a></li>
+        </ul>
+
+        <a href="#accounts-menu" class="nav-header" data-toggle="collapse"><i class="icon-briefcase"></i>账户管理</a>
+        <ul id="accounts-menu" class="nav nav-list collapse">
+            <li ><a href="users.html">用户管理</a></li>
+            <li ><a href="user.html">个人信息</a></li>
+            <li ><a href="reset-password.html">修改密码</a></li>
+        </ul>
+        <a href="help.html" class="nav-header" ><i class="icon-question-sign"></i>帮助</a>
+        <a href="faq.html" class="nav-header" ><i class="icon-comment"></i>文章</a>
+    </div>
+    <div class="content">
+    -----------------------------------------
     </div>
 </html>
 </body>
